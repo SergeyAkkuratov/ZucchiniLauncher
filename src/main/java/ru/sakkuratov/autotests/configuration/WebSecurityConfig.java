904524ffd9ba21012610268a62457ab34703af0a
Sergey Akkuratov <sergey.akkuratov@gmail.com>
2024-07-18 12:04:14 +0300

    Настройка безопасности для демонстрации

--- src/main/java/ru/sakkuratov/autotests/configuration/WebSecurityConfig.java
+++ src/main/java/ru/sakkuratov/autotests/configuration/WebSecurityConfig.java
@@ -2,9 +2,10 @@ package ru.sakkuratov.autotests.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,19 +18,28 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/", "/home").permitAll().anyRequest().authenticated())
-                .formLogin((form) -> form.loginPage("/login").permitAll()).logout(LogoutConfigurer::permitAll);
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/", "/home", "/login").permitAll().anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("admin")
-                .password("{bcrypt}$2a$10$S57Wxq3Abj6WaXq5BzY5heCrYAgaHy5ZEqIO5zGWsb8LSXR8j.za.")
-                .roles("ADMIN")
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{bcrypt}$2a$10$I2bzu0rsUI/rPlappHrJJOk78fyfebOkwJfxns6J4IWcM4PVtOvi.")
+                .roles("USER")
                 .build();
 
-        return new InMemoryUserDetailsManager(user);
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$10$I2bzu0rsUI/rPlappHrJJOk78fyfebOkwJfxns6J4IWcM4PVtOvi.")
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
